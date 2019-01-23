b9a925f687c36edb045b759ff9696dfa00882d20
Alpaslan <alpaslan@Alpaslan-Mac.local>
2019-01-23 18:30:49 +0300

    Initial Commit

---
+++ asd/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  asd
+//
+//  Created by Alpaslan on 23.01.2019.
+//  Copyright © 2019 Alpaslan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
